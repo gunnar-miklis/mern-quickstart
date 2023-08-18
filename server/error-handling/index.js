f651993cf5ebbafd259b6f32e839df8dc26de3d9
gunnar-miklis <gunnar.miklis@gmail.com>
2023-08-18 15:10:18 +0200

    update

--- server/error-handling/index.js
+++ server/error-handling/index.js
@@ -7,7 +7,7 @@ module.exports = ( app ) => {
 		console.error( 'ERROR', req.method, req.path, err );
 
 		if ( !res.headersSent ) {
-			res.status( 500 ).json( { message: 'Internal server error. Check the server console' } );
+			res.status( 500 ).json( { message: 'Internal server error.' } );
 		}
 	} );
 };
