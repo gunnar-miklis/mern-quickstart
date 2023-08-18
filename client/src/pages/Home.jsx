91f8d77cafb9bfa077d3f5632dc27c122cbde91b
gunnar-miklis <gunnar.miklis@gmail.com>
2023-08-18 15:11:39 +0200

    add styles, add example data on routes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,38 +1,46 @@
-import apiService from '../services/api';
 import { useState, useEffect } from 'react';
+import apiService from '../services/api';
+import { homeStyles } from './homeStyles';
+import { Box, Button, Link, Typography } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import RefreshIcon from '@mui/icons-material/Refresh';
+import SendIcon from '@mui/icons-material/Send';
+import GetAppIcon from '@mui/icons-material/GetApp';
+import { useParams } from 'react-router-dom';
 
 export default function Home() {
 	const [ apiResponse, setApiResponse ] = useState( {} );
+	const { pageID } = useParams();
 
 	useEffect( () => {
 		( async function getData() {
 			try {
 				const response = await apiService.getData();
-				const data = response.data;
-				setApiResponse( data );
+				setApiResponse( { data: response.data, status: response.status } );
 			} catch ( err ) {
+				setApiResponse( {} );
 				console.error( err );
 			}
 		} )();
 	}, [] );
-	function sendData() {
+	function sendData( content ) {
 		( async () => {
 			try {
-				const response = await apiService.postData();
-				const data = response.data;
-				setApiResponse( data );
+				const response = await apiService.postData( content );
+				setApiResponse( { data: response.data, status: response.status } );
 			} catch ( err ) {
+				setApiResponse( {} );
 				console.error( err );
 			}
 		} )();
 	}
-	function updateData() {
+	function updateData( content ) {
 		( async () => {
 			try {
-				const response = await apiService.updateData();
-				const data = response.data;
-				setApiResponse( data );
+				const response = await apiService.updateData( { updateID: 'updateID', content } );
+				setApiResponse( { data: response.data, status: response.status } );
 			} catch ( err ) {
+				setApiResponse( {} );
 				console.error( err );
 			}
 		} )();
@@ -40,10 +48,10 @@ export default function Home() {
 	function deleteData() {
 		( async () => {
 			try {
-				const response = await apiService.deleteData();
-				const data = response.data;
-				setApiResponse( data );
+				const response = await apiService.deleteData( 'deleteID' );
+				setApiResponse( { data: response.data, status: response.status } );
 			} catch ( err ) {
+				setApiResponse( {} );
 				console.error( err );
 			}
 		} )();
@@ -51,18 +59,57 @@ export default function Home() {
 
 	return (
 		<>
-			<h1>Home</h1>
-			<p>Hi and welcome!</p>
+			<Box sx={homeStyles.box}>
+				<Typography variant='h3'>Home</Typography>
+			</Box>
 
-			{ !Object.keys( apiResponse ).length ?
-				<h3>No API data available</h3> :
-				<h3>{ apiResponse.message }</h3>
-			}
+			<Box sx={homeStyles.box}>
+				{
+					!Object.keys( apiResponse ).length ?
+						<Typography variant='body1' color='error'>
+							No API data fetched.
+						</Typography>
+						:
+						<>
+							<Typography variant='body1' color='primary'>
+								Server Response: <em>{ apiResponse.data.message }</em>
+							</Typography>
+							<Typography variant='body1' color='primary'>
+								Status Code: { apiResponse.status }
+							</Typography>
+						</>
+				}
 
-			<a href=''><button>Get (on Page Refresh)</button></a>
-			<button onClick={()=>sendData()}>Send</button>
-			<button onClick={()=>updateData()}>Update</button>
-			<button onClick={()=>deleteData()}>Delete</button>
+				<Link href=''>
+					<Button
+						variant='outlined'
+						size='small'
+						startIcon={<GetAppIcon/>}>
+					Retrieve
+					</Button>
+				</Link>
+				<Button
+					onClick={()=>sendData( 'Client Data' )}
+					variant='outlined'
+					size='small'
+					startIcon={<SendIcon/>}>
+				Send
+				</Button>
+				<Button
+					onClick={()=>updateData( 'Client Data' )}
+					variant='outlined'
+					size='small'
+					startIcon={<RefreshIcon/>}>
+				Update
+				</Button>
+				<Button
+					onClick={()=>deleteData()}
+					variant='outlined'
+					size='small'
+					startIcon={<DeleteIcon/>}>
+				Delete
+				</Button>
+			</Box>
 		</>
 	);
 }
